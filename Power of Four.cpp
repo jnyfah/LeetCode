83c11c52b5e5604ae23086cb15e6713515c7abf8
Jennifer Chukwu <jnyfaah@gmail.com>
2020-09-02 12:29:35 +0100

    Create Power of Four.cpp

---
+++ Power of Four.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        
+        
+        
+        if(num <= 0)
+        {
+            return false;
+        }else if(num == 1)
+        {
+            return true;
+        }
+        else{
+
+         return !(num & (num-1)) && (num % 3 ==1);
+        }
+       
+        
+    }
+};
