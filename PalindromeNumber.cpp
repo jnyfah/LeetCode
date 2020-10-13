99770188b87b8dd32ef99525ffd986b1973cd1b6
Jennifer Chukwu <jnyfaah@gmail.com>
2020-10-14 00:53:53 +0100

    Update PalindromeNumber.cpp

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -1,18 +1,16 @@
 class Solution {
-public:
-    
-      bool isPalindrome(int x) {
-    if(x>=2147483647 || x<=-2147483648)
-         {
-             return false;
-         }
-    int y=x,i=0;
-    while(x>0)
-    {
-    i=i*10+x%10;
-    x=x/10;    
+  public:
+
+    bool isPalindrome(int x) {
+      if (x >= 2147483647 || x <= -2147483648) {
+        return false;
+      }
+      int y = x, i = 0;
+      while (x > 0) {
+        i = i * 10 + x % 10;
+        x = x / 10;
+      }
+      return (i == y ? true : false);
     }
-    return(i==y?true:false);
-    }
-    
+
 };
