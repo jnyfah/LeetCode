7e8f689e37d1d9bc4fc14263f47b631ecd5fbe3f
Jennifer Chukwu <jnyfaah@gmail.com>
2020-10-14 00:43:47 +0100

    Update Most Frequent Subtree Sum.cpp

--- Most Frequent Subtree Sum.cpp
+++ Most Frequent Subtree Sum.cpp
@@ -10,52 +10,37 @@
  * };
  */
 class Solution {
-    private:
-    map<int, int> m;
-    vector<int> v;
-    
-        
-        int subtree(TreeNode* root, int& freq)
-        {
-            int left, right, sum = 0;
-            
-           if(!root) 
-           {
-               return 0;
-           }else
-           {
-               left = subtree(root->left, freq);
-               right = subtree(root->right, freq);
-               sum = left + right + root->val;
-            
-               m[sum]++;
-            
-               if (m[sum] > freq)
-                 {
-                    freq = m[sum];
-            
-                     v.clear();
-            
-                     v.push_back(sum);
-                   
-                  } else if (m[sum] == freq)
-                  {
-                      v.push_back(sum);
-                   }
-           
-               }
-            
-        
-        return sum;
+  private:
+    map < int, int > m;
+    vector < int > v;
+    int subtree(TreeNode * root, int & freq) {
+    int left, right, sum = 0;
+
+    if (!root) {
+      return 0;
+    } else {
+      left = subtree(root -> left, freq);
+      right = subtree(root -> right, freq);
+      sum = left + right + root -> val;
+
+      m[sum]++;
+
+      if (m[sum] > freq) {
+        freq = m[sum];
+        v.clear();
+        v.push_back(sum);
+
+      } else if (m[sum] == freq) {
+        v.push_back(sum);
+      }
     }
-public:
-    vector<int> findFrequentTreeSum(TreeNode* root) 
-    {
-        int freq=0;
-        
-        subtree(root, freq);
-        
-        return v;
-        
+    return sum;
+  }
+  public:
+    vector < int > findFrequentTreeSum(TreeNode * root) {
+      int freq = 0;
+      subtree(root, freq);
+      return v;
+
     }
 };
