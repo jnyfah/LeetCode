ce805a12ae3028e35cdef9a6f9465e71f8c245cd
Jennifer Chukwu <jnyfaah@gmail.com>
2020-11-03 05:44:31 +0100

    Create 137. Single Number II.cpp
    Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one

---
+++ 137. Single Number II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        std::map<int, int>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto m:mp){
+            if(m.second == 1){
+                return m.first;
+            }
+        }
+        return 0;
+}
+};
