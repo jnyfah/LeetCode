db2e0982a7efe05afbdfe5a23c662391f2d2dc83
Jennifer Chukwu <jnyfaah@gmail.com>
2021-01-27 15:15:04 +0100

    Create 278. First Bad Version.cpp

---
+++ 278. First Bad Version.cpp
@@ -0,0 +1,25 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int start = 1, end =n;
+        
+        
+        while(start < end){
+            int mid =start + (end - start)/2;
+            
+            if(isBadVersion(mid)){
+               
+                end =mid;
+            }else {
+                start = mid+1;
+            }
+            
+        }
+        
+       return start; 
+        
+    }
+};
