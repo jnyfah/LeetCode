f3c9a27b863d5941b63d998eae88cfeeb832f93a
Jennifer Chukwu <jnyfaah@gmail.com>
2021-06-21 15:09:47 +0100

    Create 61. Rotate List.cpp

---
+++ 61. Rotate List.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head) return head;
+        
+        int len=1; // number of nodes
+        ListNode *newH, *tail;
+        newH=tail=head;
+        
+        while(tail->next)  // get the number of nodes in the list
+        {
+            tail = tail->next;
+            len++;
+        }
+        tail->next = head; // circle the link
+
+        if(k %= len) 
+        {
+            for(auto i=0; i<len-k; i++) {
+                tail = tail->next;
+            }
+                
+        }
+        newH = tail->next; 
+        tail->next = NULL;
+        return newH;
+    }
+};
