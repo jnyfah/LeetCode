a67554b31006a5b643dce5fc2de7197fc13ac389
Jennifer Chukwu <jnyfaah@gmail.com>
2021-08-03 13:51:13 +0100

    Update 509. Fibonacci Number.cpp

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    
+  
     int fib(int n) {
-        if (n <= 1) {
+        std::vector<int>fibs(n+1);
+        
+        if(n == 0 || n ==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        
+        fibs[0] = 0;
+        fibs[1] = 1;
+        
+        for(int i = 2; i < n+1; i ++) {
+            fibs[i] = fibs[i-1] + fibs[i-2];
+        }
+        
+        return fibs[n];
     }
 };
