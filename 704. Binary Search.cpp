c0290ddb67f2657728e85b33025f1c1dc4d6e0fd
Jennifer Chukwu <jnyfaah@gmail.com>
2021-08-12 02:53:42 +0100

    Create 704. Binary Search.cpp

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size()-1, i = -1;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            
+            if(target == nums[mid]) {
+                return mid;
+            }
+            
+            else if(target > nums[mid]) {
+                start = mid +1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+};
