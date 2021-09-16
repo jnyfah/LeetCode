e5ef89fe6b6b81fc43dc7b19d65da261d5b805b7
Jennifer Chukwu <jnyfaah@gmail.com>
2021-09-16 21:18:30 +0100

    Create DAY16.cpp

---
+++ HACKER RANK 30 DAYS OF CODE/DAY16.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string S;
+    getline(cin, S);
+    try
+    {
+        int no;
+        no=stoi(S);
+        cout<<no<<endl;
+    }
+    catch(exception a)
+    {
+        cout<<"Bad String";
+    }
+    return 0;
+
+    return 0;
+}
