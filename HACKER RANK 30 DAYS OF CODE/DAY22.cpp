0eedb38d87bae33efb50d777244523ed20e887b2
Jennifer Chukwu <jnyfaah@gmail.com>
2021-09-22 12:11:57 +0100

    Create DAY22.cpp

---
+++ HACKER RANK 30 DAYS OF CODE/DAY22.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+          //Write your code here
+          //Write your code here
+          if(!root) {
+              return -1;
+          }
+          int leftDepth = getHeight(root->left);
+          int rightDepth = getHeight(root->right);
+              
+          return std::max(leftDepth, rightDepth)+ 1;
+
+        }
+
+}; //End of Solution
