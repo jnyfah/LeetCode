e8c323830eff2dc69511c5cc812e20a0360df154
Jennifer Chukwu <jnyfaah@gmail.com>
2021-10-18 14:00:07 +0100

    Create 1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp

---
+++ 1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        
+        if(target.size() != arr.size()) {
+            return false;
+        }
+        std::sort(target.begin(), target.end());
+        std::sort(arr.begin(), arr.end());
+        
+        return target == arr;
+    }
+};
