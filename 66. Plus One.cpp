5c8637b3460fe61de7f3f23512c55232714725fd
Jennifer Chukwu <jnyfaah@gmail.com>
2021-12-07 22:52:29 +0100

    Update 66. Plus One.cpp

--- 66. Plus One.cpp
+++ 66. Plus One.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-         vector<int> res;
-        int length = digits.size();
-        int carry = 1;
-        int temp;
-        for(int i = length - 1; i >= 0; --i)
-        {
-            temp = digits[i] + carry;
-            carry = temp > 9 ? 1 : 0;
-            temp %= 10;
-            res.push_back(temp);
-        }
-        if(1 == carry)
-        {
-            res.push_back(carry);
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        int n = digits.size()-1;
         
+        if(digits.size() == 1 && digits[0] !=9) {
+            digits[0]++;
+            return digits;
+        }
+        
+        for(int i = n; i>=0; i--) {
+            if(digits[i] == 9){
+                digits[i] = 0;
+            } else {
+                digits[i] ++;
+                return digits;
+            }
+            
+            
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
