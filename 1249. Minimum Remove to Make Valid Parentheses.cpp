9939a09e15d4f15cd7641ec94ad36cdcbd1d59a9
Jennifer Chukwu <jnyfaah@gmail.com>
2021-12-13 05:06:42 +0100

    Create 1249. Minimum Remove to Make Valid Parentheses.cpp

---
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        std::stack<int>st;
+        
+        for(int i=0; i< s.size(); i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            }else if(s[i] == ')'){
+                if(st.empty()) {
+                    s[i] = '*';
+                }else {
+                    st.pop();
+                }
+            }
+        }
+        while(!st.empty()){
+            s[st.top()]='*';
+            st.pop();
+        }
+        s.erase(remove(s.begin(), s.end(),'*'), s.end());
+        return s;
+    }
+};
