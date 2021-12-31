8ad20a1b4d283165f84403b72639526caf2945d9
Jennifer Chukwu <jnyfaah@gmail.com>
2021-12-31 13:33:56 +0100

    Create 60. Subarray Sum Equals K.cpp

---
+++ 60. Subarray Sum Equals K.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        unordered_map<int, int>mp;
+        int sum = 0; int count = 0;
+        mp[0]++;
+        
+        for(int i = 0; i<nums.size(); i++) {
+            sum += nums[i];
+            
+            if(mp.find(sum-k) != mp.end()) {
+                count += mp[sum-k];
+            }
+            
+            mp[sum]++;
+            
+        }
+        
+        return count;
+    }
+};
