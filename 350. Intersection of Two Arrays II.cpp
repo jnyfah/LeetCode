f021cc52672c14c55a83786031dc00cb024ac624
Jennifer Chukwu <jnyfaah@gmail.com>
2022-01-03 11:21:52 +0100

    Update 350. Intersection of Two Arrays II.cpp

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        std::sort(nums1.begin(), nums1.end());
-        std::sort(nums2.begin(), nums2.end());
-        std::vector<int>intersect;
-        
-        std::set_intersection(nums1.begin(), nums1.end(),
-                              nums2.begin(), nums2.end(),
-                              std::inserter(intersect, intersect.begin()));
-        return intersect;
+        map<int,int>freq;
+        vector<int>ans;
+        for(int i = 0;i<nums1.size();i++){
+            freq[nums1[i]]++;
+        }
+        for(int i = 0;i<nums2.size();i++){
+            if (freq[nums2[i]] > 0){
+                freq[nums2[i]]--; 
+                ans.push_back(nums2[i]);
+            }
+        }
+        return ans;
     }
 };
