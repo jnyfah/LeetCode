63c9444f53c33e425fc88f7f479b87ac64ab5660
Jennifer Chukwu <jnyfaah@gmail.com>
2022-01-27 19:55:52 +0100

    Update 27. Remove Element.cpp

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-       int i =0;
-       while (i < nums.size()){
-         if(nums[i] == val){
-           nums.erase(nums.begin() + i);
-         }else{
-           i++;
-         }
-       }
-       return nums.size(); 
+        int i = 0; 
+        for(int j = 0; j<nums.size(); j++) {
+            if(nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 };
