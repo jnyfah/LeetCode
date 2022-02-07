5f549f39762fba9183b41abe01112fda5e4e1279
Jennifer Chukwu <jnyfaah@gmail.com>
2022-02-07 22:59:44 +0100

    Update 74. Search a 2D Matrix.cpp

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -1,66 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        
-        if (matrix.empty() || matrix[0].empty())
-            return false;
-        
-        int rowLength = matrix.size();
-        int columnLength = matrix[0].size();
-        
-        int row = 0;
-        
-        cout << "rowLength: "    << rowLength    << endl;
-        cout << "columnLength: " << columnLength << endl;
-        
-        int rowStart = 0, rowMid = 0, rowEnd = rowLength-1;
-        
-        while (rowStart <= rowEnd) {
-            
-            rowMid = rowStart + (rowEnd - rowStart) / 2;
-            
-            if (matrix[rowMid][0] == target){
-                return true;
-            }
-            else if (matrix[rowMid][0] < target){
-                
-                if (matrix[rowMid][columnLength-1] >= target) {
-                    row = rowMid;
-                    break;
-                }
-                else {
-                    rowStart = rowMid + 1;
-                }
-            }
-            else if (matrix[rowMid][0] > target){
-                if ( (rowMid - 1 >= 0) && matrix[rowMid-1][0] <= target) {
-                    row = rowMid-1;
-                    break;
-                }
-                else {
-                    rowEnd = rowMid-1;
-                }
-            }
-            
-        }
-        
-        int columnStart = 0, columnMid = 0, columnEnd = columnLength-1;
-        while (columnStart <= columnEnd) {
-            
-            columnMid = columnStart + (columnEnd - columnStart) / 2;
-            
-            if (matrix[row][columnMid] == target) {
-                return true;
-            }
-            else if (matrix[row][columnMid] < target) {
-                columnStart = columnMid+1;
-            }
-            else if (matrix[row][columnMid] > target) {
-                columnEnd = columnMid-1; 
-            }
-        }
-        
+    if(matrix.empty() || matrix[0].empty()) {
         return false;
+    }
+    int m = matrix.size(), n = matrix[0].size();
+    int start = 0, end = m*n - 1;
+    while(start <= end) {
+        int mid = start + (end - start)/2;
+        int e = matrix[mid/n][mid%n];
+        if(target < e) {
+            end = mid - 1;
+        }
+        else if(target > e) {
+            start = mid + 1;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
     }
 };
