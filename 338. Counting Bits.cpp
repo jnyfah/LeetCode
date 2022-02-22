fc72157a209c526e5b9e31aaf3e5e9ea7ebebda1
Jennifer Chukwu <jnyfaah@gmail.com>
2022-02-22 23:28:48 +0100

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    vector<int> countBits(int num) {
-        vector<int> dp(num+1);
-        int powerOf2 = 1;
-        for ( int i = 1; i <= num; i++ )
-        {
-            if ( i == 2*powerOf2 )
-                powerOf2 *= 2;
-            dp[i] = dp[i-powerOf2] + 1;
+    vector<int> countBits(int n) {
+        vector<int> res;
+        res.push_back(0);
+        
+        for(int i =1; i<= n; i++) {
+            int temp = res[i/2] + (i%2);
+            res.push_back(temp);
         }
-        return dp;
+        return res;
     }
 };
