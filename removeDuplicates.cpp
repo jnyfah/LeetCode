2857cc214032159c5c11fdb4089e9990b19a1e0f
Jennifer Chukwu <jnyfaah@gmail.com>
2022-03-27 22:09:27 +0100

    Delete removeDuplicates.cpp

--- removeDuplicates.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-      auto last = std::unique(nums.begin(), nums.end());
-      nums.erase(last, nums.end());
-      return nums.size();
-    }
-};
